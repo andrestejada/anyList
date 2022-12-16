8f74031f97f6333406d24df5af51774ec7947324
Andres <aneduardotejada@gmail.com>
2022-12-15 21:37:22 -0500

    seed items

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,33 +3,48 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Item } from 'src/items/entities/item.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
-import { SEED_USERS } from './data/seed-data';
+import { SEED_USERS, SEED_ITEMS } from './data/seed-data';
 import { UsersService } from '../users/users.service';
+import { ItemsService } from 'src/items/items.service';
 @Injectable()
 export class SeedService {
   constructor(
     @InjectRepository(Item) private itemRepository: Repository<Item>,
     @InjectRepository(User) private userRepository: Repository<User>,
     private usersService: UsersService,
+    private itemService: ItemsService,
   ) {}
 
   async executeSeed() {
     await this.deleteDataBase();
 
-    await this.loadUsers();
-
+    const users = await this.loadUsers();
+    await this.loadItems(users);
     return true;
   }
 
   async loadUsers() {
-    const users = [];
+    const users: User[] = [];
 
     for (const user of SEED_USERS) {
       users.push(await this.usersService.create(user));
     }
-    return users[0];
+    return users;
   }
-
+  async loadItems(users: User[]) {
+    const itemsPromise = [];
+    let counter = 0;
+    for (const item of SEED_ITEMS) {
+      if (counter >= SEED_USERS.length) {
+        counter = 0;
+      }
+      itemsPromise.push(this.itemService.create(item, users[counter]));
+      counter++;
+    }
+    
+    const res = await Promise.all(itemsPromise);
+    console.log(res)
+  }
   async deleteDataBase() {
     await this.itemRepository.createQueryBuilder().delete().where({}).execute();
     await this.userRepository.createQueryBuilder().delete().where({}).execute();
